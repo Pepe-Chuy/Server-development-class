f582effaf144489d68dcbd763f2b49164692454c
Pepe Chuy <dosjuegos6@gmail.com.com>
2026-02-13 01:07:07 -0600

    performance

--- tarea1/4_ArrayBenchmark.js
+++ tarea1/4_ArrayBenchmark.js
@@ -11,23 +11,28 @@ function execBenchmark(size){
     const data = createArray(size);
 
     // filter + map
-    console.time("filter + map");
+    let start = performance.now();
+
     const result1 = data.filter(n => n % 2 === 0).map(n => n * 2);
-    console.timeEnd("filter + map")
+    
+    let filterMapTime = performance.now() - start;
 
     // reduce
-    console.time("reduce");
+    let start = performance.now();   
+    
     const result2 = data.reduce((acc, n) => {
     if (n % 2 === 0) {
       acc.push(n * 2);
     }
     return acc;
-    });
+    }, []);
     console.timeEnd("reduce");
 
+    let reduceTime = performance.now() - start;
 
     // for loop
-    console.time("for loop");
+    let start = performance.now();
+    
     const result3 = [];
     for (let i = 0; i < data.length; i++) {
         if (data[i] % 2 === 0) {
@@ -35,19 +40,20 @@ function execBenchmark(size){
         }
     }
     console.timeEnd("for loop");
-
+    
+    let forTime = performance.now() - start;
 
     // validamos
-    console.log("Resultados:",
-    result1.length === result2.length &&
-    result2.length === result3.length
-  );
+    console.log("Resultados:");
+    console.log(`filter + map = ${filterMapTime.toFixed(3)} ms`);
+    console.log(`reduce       = ${reduceTime.toFixed(3)} ms`);
+    console.log(`for loop     = ${forTime.toFixed(3)} ms`);
 }
 
 
 
 ///// USO
 
-runBenchmark(10_000);
-runBenchmark(100_000);
-runBenchmark(1_000_000);
+execBenchmark(10_000);
+execBenchmark(100_000);
+execBenchmark(1_000_000);
