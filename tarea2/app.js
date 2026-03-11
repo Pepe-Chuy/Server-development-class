f6ebbfccf755aa911022ba2e3fa4472c07c7a398
Pepe Chuy <dosjuegos6@gmail.com.com>
2026-03-11 13:32:01 -0600

    added views

--- tarea2/app.js
+++ tarea2/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+// Midlewares
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// Routes
+
+// Get CV page
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 function salute(req, res) {
   res.send("hello world");
