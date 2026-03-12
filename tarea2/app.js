e873b73034fc772f40f7b720ba8b96122536b003
Pepe Chuy <dosjuegos6@gmail.com.com>
2026-03-11 22:58:11 -0600

    defines routes

--- tarea2/app.js
+++ tarea2/app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Midlewares
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // Routes
 
 // Get CV page
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-function salute(req, res) {
-  res.send("hello world");
-}
+// Get contacto 
+app.get("/contacto", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "contact.html"));
+})
 
-app.get("/", salute);
-
-app.listen(3000);
